1dbbdd261095a5bc5f03410755f556dc66c211dd
Rully Ihza Mahendra <rullyihza00@gmail.com>
2021-10-16 14:19:50 +0700

    Creating tv show model files

---
+++ lib/domain/entities/tv_show_detail.dart
@@ -0,0 +1,39 @@
+import 'package:ditonton/domain/entities/genre.dart';
+import 'package:equatable/equatable.dart';
+
+class TVShowDetail extends Equatable {
+  TVShowDetail(
+      {required this.backdropPath,
+      required this.firstAirDate,
+      required this.genres,
+      required this.id,
+      required this.name,
+      required this.overview,
+      required this.posterPath,
+      required this.voteAverage,
+      required this.voteCount});
+
+  final String? backdropPath;
+  final String firstAirDate;
+  final List<Genre> genres;
+  final int id;
+  final String name;
+  final String overview;
+  final String posterPath;
+  final double voteAverage;
+  final int voteCount;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        backdropPath,
+        firstAirDate,
+        genres,
+        id,
+        name,
+        overview,
+        posterPath,
+        voteAverage,
+        voteCount
+      ];
+}
