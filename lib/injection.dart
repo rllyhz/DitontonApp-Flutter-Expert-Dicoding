14e653ea220f6ddd561da6a6a4d82966fc75652d
Rully Ihza Mahendra <rullyihza00@gmail.com>
2021-10-18 15:32:53 +0700

    Adding GetNowPlayingTVShows, GetPopularTVShows, GetTopRatedTVShows usecases to injection and add tv_show_list_notifier to injection file

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,17 +1,25 @@
 import 'package:ditonton/data/datasources/db/database_helper.dart';
 import 'package:ditonton/data/datasources/movie_local_data_source.dart';
 import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
+import 'package:ditonton/data/datasources/tv_show_local_data_source.dart';
+import 'package:ditonton/data/datasources/tv_show_remote_data_source.dart';
 import 'package:ditonton/data/repositories/movie_repository_impl.dart';
+import 'package:ditonton/data/repositories/tv_show_repository_impl.dart';
 import 'package:ditonton/domain/repositories/movie_repository.dart';
+import 'package:ditonton/domain/repositories/tv_show_repository.dart';
+import 'package:ditonton/domain/usecases/tv_show/get_now_playing_tv_shows.dart';
+import 'package:ditonton/domain/usecases/tv_show/get_popular_tv_shows.dart';
+import 'package:ditonton/domain/usecases/tv_show/get_top_rated_tv_shows.dart';
 import 'package:ditonton/presentation/provider/home_notifier.dart';
 import 'package:ditonton/presentation/provider/movie_detail_notifier.dart';
 import 'package:ditonton/presentation/provider/movie_list_notifier.dart';
 import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
 import 'package:ditonton/presentation/provider/popular_movies_notifier.dart';
 import 'package:ditonton/presentation/provider/top_rated_movies_notifier.dart';
+import 'package:ditonton/presentation/provider/tv_show_list_notifier.dart';
 import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
-import 'package:http/http.dart' as http;
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
 
 import 'domain/usecases/movie/get_movie_detail.dart';
 import 'domain/usecases/movie/get_movie_recommendations.dart';
@@ -33,6 +41,13 @@ void init() {
       getNowPlayingMovies: locator(),
       getPopularMovies: locator(),
       getTopRatedMovies: locator(),
+    ),
+  );
+  locator.registerFactory(
+    () => TVShowListNotifier(
+      getNowPlayingTVShows: locator(),
+      getPopularTVShows: locator(),
+      getTopRatedTVShows: locator(),
     ),
   );
   locator.registerFactory(
@@ -70,8 +85,11 @@ void init() {
 
   // use case
   locator.registerLazySingleton(() => GetNowPlayingMovies(locator()));
+  locator.registerLazySingleton(() => GetNowPlayingTVShows(locator()));
   locator.registerLazySingleton(() => GetPopularMovies(locator()));
+  locator.registerLazySingleton(() => GetPopularTVShows(locator()));
   locator.registerLazySingleton(() => GetTopRatedMovies(locator()));
+  locator.registerLazySingleton(() => GetTopRatedTVShows(locator()));
   locator.registerLazySingleton(() => GetMovieDetail(locator()));
   locator.registerLazySingleton(() => GetMovieRecommendations(locator()));
   locator.registerLazySingleton(() => SearchMovies(locator()));
@@ -80,9 +98,15 @@ void init() {
   locator.registerLazySingleton(() => RemoveWatchlist(locator()));
   locator.registerLazySingleton(() => GetWatchlistMovies(locator()));
 
-  // repository
+  // repositories
   locator.registerLazySingleton<MovieRepository>(
     () => MovieRepositoryImpl(
+      remoteDataSource: locator(),
+      localDataSource: locator(),
+    ),
+  );
+  locator.registerLazySingleton<TVShowRepository>(
+    () => TVShowRepositoryImpl(
       remoteDataSource: locator(),
       localDataSource: locator(),
     ),
@@ -91,8 +115,12 @@ void init() {
   // data sources
   locator.registerLazySingleton<MovieRemoteDataSource>(
       () => MovieRemoteDataSourceImpl(client: locator()));
+  locator.registerLazySingleton<TVShowRemoteDataSource>(
+      () => TVShowRemoteDataSourceImpl(client: locator()));
   locator.registerLazySingleton<MovieLocalDataSource>(
       () => MovieLocalDataSourceImpl(databaseHelper: locator()));
+  locator.registerLazySingleton<TVShowLocalDataSource>(
+      () => TVShowLocalDataSourceImpl(databaseHelper: locator()));
 
   // helper
   locator.registerLazySingleton<DatabaseHelper>(() => DatabaseHelper());
